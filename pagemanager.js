3958ca8b943b6b719eec010bb24c80f9a860ed85
stefanarctic <87850525+stefanarctic@users.noreply.github.com>
2022-01-27 20:03:20 +0200

    Added downloading

--- pagemanager.js
+++ pagemanager.js
@@ -38,3 +38,21 @@ export const GetAbsolutePath = () => {
     arr.pop();
     return arr.join("") + window.location.pathname;
 }
+
+export const download = (data, filename, type) => {
+    const file = new Blob([data], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        const a = document.createElement("a"),
+        url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+}
