f4373994fa1a295789c4abb67984a37c83069a40
stefanarctic <87850525+stefanarctic@users.noreply.github.com>
2022-01-27 19:34:59 +0200

    Created pagemanager.js

---
+++ pagemanager.js
@@ -0,0 +1,40 @@
+/*
+*
+* Copyright Stefanarctic (C)
+*
+*/
+
+export const getPageProperties = () => {
+    let properties = document.location.search;
+    properties = properties.replace("?", "");
+    const variables = properties.split("&");
+    const arr = [];
+    for(const variable of variables) {
+        arr.push(variable.replace("%20", " ").split("="));
+    }
+    return arr;
+}
+
+export function GetSiteRoot() {
+    let rootPath = window.location.protocol + "//" + window.location.host + "/";
+    if (window.location.hostname == "localhost")
+    {
+        var path = window.location.pathname;
+        if (path.indexOf("/") == 0)
+        {
+            path = path.substring(1);
+        }
+        path = path.split("/", 1);
+        if (path != "")
+        {
+            rootPath += path + "/";
+        }
+    }
+    return rootPath;
+}
+
+export const GetAbsolutePath = () => {
+    let arr = GetSiteRoot().split("");
+    arr.pop();
+    return arr.join("") + window.location.pathname;
+}
